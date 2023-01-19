1cbaac9a31b183b233de514e0f5a30a5c66013ee
Danila Romanov <danila.romanof@gmail.com>
2023-01-19 22:22:30 +0200

    Liquibase migration tool and it's implementation

--- src/main/java/config/DbConfig.java
+++ src/main/java/config/DbConfig.java
@@ -1,19 +1,26 @@
 package config;
 
 import jakarta.persistence.EntityManagerFactory;
+import liquibase.integration.spring.SpringLiquibase;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 @ComponentScan(basePackages = "config.database")
 public class DbConfig {
+    @Value("${liquibase.changeLog-path}")
+    private String liquibaseChangelogPath;
+
     @Bean
     public EntityManagerFactory entityManagerFactory(@Qualifier("dialect") String dialect, DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
@@ -36,5 +43,13 @@ public class DbConfig {
         properties.setProperty("hibernate.format_sql", "true");
 
         return properties;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(liquibaseChangelogPath);
+        liquibase.setDataSource(dataSource);
+        return liquibase;
     }
 }
