a9d29a3fe486c3e6d6bda3b0bbf2ab0c7c797302
Danila Romanov <danila.romanof@gmail.com>
2023-01-19 22:44:31 +0200

    GetUserRepositoryImpl function

--- src/main/java/repository/user/GetUserRepositoryImpl.java
+++ src/main/java/repository/user/GetUserRepositoryImpl.java
@@ -1,13 +1,19 @@
 package repository.user;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import model.user.User;
 import org.springframework.stereotype.Repository;
 import service.user.get_use_case.GetUserRepository;
 
 @Repository
 public class GetUserRepositoryImpl implements GetUserRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        return entityManager.find(User.class, username);
     }
 }
