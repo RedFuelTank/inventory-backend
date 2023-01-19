e79b315156d6774574b22afd565b090632433c6f
Danila Romanov <danila.romanof@gmail.com>
2023-01-19 20:16:04 +0200

    ExceptionAdvice functionality

---
+++ src/main/java/exception/ExceptionAdvice.java
@@ -0,0 +1,12 @@
+package exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler
+    public String onException() {
+        return "internal error!";
+    }
+}
