95da7ee25b048c72803c0b337b48085f6842eb19
Danila Romanov <danila.romanof@gmail.com>
2023-01-19 20:32:54 +0200

    ExceptionAdvice functionality

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -6,5 +6,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"controller"})
+@ComponentScan(basePackages = {"controller", "exception"})
 public class MvcConfig {}
