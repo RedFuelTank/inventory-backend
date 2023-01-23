c002fd852306ea7059c86d01ab529a49d9fcdf78
Danila Romanov <danila.romanof@gmail.com>
2023-01-23 17:00:11 +0200

    GetDirectoryController pageable parameter

--- src/main/java/controller/directory/get_use_case/GetDirectoryController.java
+++ src/main/java/controller/directory/get_use_case/GetDirectoryController.java
@@ -3,11 +3,10 @@ package controller.directory.get_use_case;
 import dto.directory.EntityDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,12 +14,12 @@ public class GetDirectoryController {
     private final GetDirectoryService service;
 
     @GetMapping("{username}/directory/root")
-    public Page<EntityDto> getUserRootDirectoryContent(@PathVariable String username) {
-        return service.getUserRootDirectoryContent(username);
+    public Page<EntityDto> getUserRootDirectoryContent(@PathVariable String username, Pageable pageable) {
+        return service.getUserRootDirectoryContent(username, pageable);
     }
 
     @GetMapping("{username}/directory/{id}")
-    public Page<EntityDto> getUserDirectoryByIdContent(@PathVariable String username, @PathVariable Long id) {
-        return service.getUserDirectoryByIdContent(username, id);
+    public Page<EntityDto> getUserDirectoryByIdContent(@PathVariable String username, @PathVariable Long id, Pageable pageable) {
+        return service.getUserDirectoryByIdContent(username, id, pageable);
     }
 }
--- src/main/java/controller/directory/get_use_case/GetDirectoryService.java
+++ src/main/java/controller/directory/get_use_case/GetDirectoryService.java
@@ -2,12 +2,13 @@ package controller.directory.get_use_case;
 
 import dto.directory.EntityDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface GetDirectoryService {
     @PreAuthorize("#username == authentication.name")
-    Page<EntityDto> getUserRootDirectoryContent(String username);
+    Page<EntityDto> getUserRootDirectoryContent(String username, Pageable pageable);
 
     @PreAuthorize("#username == authentication.name")
-    Page<EntityDto> getUserDirectoryByIdContent(String username, Long id);
+    Page<EntityDto> getUserDirectoryByIdContent(String username, Long id, Pageable pageable);
 }
