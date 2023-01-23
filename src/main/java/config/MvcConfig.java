e8776eba61a76143b2d7becdd342c4feb515daca
Danila Romanov <danila.romanof@gmail.com>
2023-01-23 18:18:51 +0200

    Spring Data JPA and Pageable functionality have been enabled

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -2,9 +2,13 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@EnableJpaRepositories(basePackages = "service")
+@EnableSpringDataWebSupport
 @ComponentScan(basePackages = {"controller", "exception", "service", "repository"})
 public class MvcConfig {}
