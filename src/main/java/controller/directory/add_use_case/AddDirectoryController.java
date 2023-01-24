cca311c64f52e28c81b04c712130dbacaa58b56a
Danila Romanov <danila.romanof@gmail.com>
2023-01-24 17:58:10 +0200

    AddDirectoryController new function for adding item into root directory

--- src/main/java/controller/directory/add_use_case/AddDirectoryController.java
+++ src/main/java/controller/directory/add_use_case/AddDirectoryController.java
@@ -4,19 +4,22 @@ import dto.directory.DirectoryDto;
 import dto.directory.EntityDto;
 import dto.directory.ItemDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller
 @RequiredArgsConstructor
 public class AddDirectoryController {
     private final AddDirectoryService service;
 
     @PostMapping("{username}/directory/root")
-    public EntityDto addUserRootDirectory(@PathVariable String username, @RequestBody DirectoryDto directoryDto) {
-        return service.addUserRootDirectory(username, directoryDto);
+    public EntityDto addUserRootDirectorySubDirectory(@PathVariable String username, @RequestBody DirectoryDto directoryDto) {
+        return service.addUserRootDirectorySubDirectory(username, directoryDto);
+    }
+
+    @PostMapping("{username}/directory/root")
+    public EntityDto addUserRootDirectorySubItem(@PathVariable String username, @RequestBody ItemDto itemDto) {
+        return service.addUserRootDirectorySubItem(username, itemDto);
     }
 
     @PostMapping("{username}/parent-directory/{id}/create-directory")
