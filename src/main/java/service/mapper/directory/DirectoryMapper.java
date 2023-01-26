d7c5f3149c90888fd75d2e374f6c33ec04ce2c97
Danila Romanov <danila.romanof@gmail.com>
2023-01-26 10:55:44 +0200

    Change mappers methods for new DTO and Entity requirements

--- src/main/java/service/mapper/directory/DirectoryMapper.java
+++ src/main/java/service/mapper/directory/DirectoryMapper.java
@@ -2,19 +2,35 @@ package service.mapper.directory;
 
 import dto.directory.DirectoryDto;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import model.directory.Directory;
+import model.directory.Item;
+import model.user.User;
 import org.springframework.stereotype.Component;
 import service.mapper.Mapper;
+import service.user.get_use_case.GetUserRepository;
 
 @Component
+@RequiredArgsConstructor
 public class DirectoryMapper implements Mapper<DirectoryDto, Directory> {
+    private final GetUserRepository userRepository;
+
     @Override
     public DirectoryDto toDto(Directory directory) {
-        return new DirectoryDto(directory.getName(), directory.getUser().getUsername());
+        return new DirectoryDto(
+                directory.getId(),
+                directory.getName(),
+                directory.getUser().getUsername(),
+                directory.getUpperId());
     }
 
     @Override
     public Directory toEntity(DirectoryDto directoryDto) {
-        return null;
+        Directory directory = new Directory();
+        User user = userRepository.getUserByUsername(directoryDto.getUsername());
+        directory.setName(directoryDto.getName());
+        directory.setUpperId(directoryDto.getParentDirectoryId());
+        directory.setUser(user);
+        return directory;
     }
 }
--- src/main/java/service/mapper/directory/ItemMapper.java
+++ src/main/java/service/mapper/directory/ItemMapper.java
@@ -2,19 +2,33 @@ package service.mapper.directory;
 
 import dto.directory.ItemDto;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import model.directory.Item;
+import model.user.User;
 import org.springframework.stereotype.Component;
 import service.mapper.Mapper;
+import service.user.get_use_case.GetUserRepository;
 
 @Component
+@RequiredArgsConstructor
 public class ItemMapper implements Mapper<ItemDto, Item> {
+    private final GetUserRepository userRepository;
     @Override
     public ItemDto toDto(Item item) {
-        return new ItemDto(item.getName(), item.getUser().getUsername());
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getUser().getUsername(),
+                item.getDirectoryId());
     }
 
     @Override
     public Item toEntity(ItemDto itemDto) {
-        return null;
+        Item item = new Item();
+        User user = userRepository.getUserByUsername(itemDto.getUsername());
+        item.setName(itemDto.getName());
+        item.setDirectoryId(item.getDirectoryId());
+        item.setUser(user);
+        return item;
     }
 }
