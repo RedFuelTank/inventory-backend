283d2d1d9b8a9c76391829feb80b4581060e45d4
Danila Romanov <danila.romanof@gmail.com>
2023-01-27 19:30:49 +0200

    Add username for GetStatisticsRepository

--- src/main/java/service/statistics/get_use_case/GetStatisticsRepository.java
+++ src/main/java/service/statistics/get_use_case/GetStatisticsRepository.java
@@ -5,12 +5,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GetStatisticsRepository extends JpaRepository<StatisticsUnit, Long> {
 
-    @Query(value = "select o from StatisticsUnit o where o.startDate = :from and o.endDate = :to")
-    Page<StatisticsUnit> getStatisticsFromTo(String username, Long from, Long to, Pageable pageable);
+    @Query(value = "select o from StatisticsUnit o where o.user.username = :username and o.startDate >= :from and o.endDate <= :to")
+    Page<StatisticsUnit> getStatisticsFromTo(@Param("username") String username,
+                                             @Param("from") Long from,
+                                             @Param("to") Long to,
+                                             Pageable pageable);
 
-    @Query(value = "select o from StatisticsUnit o where o.startDate = :from and o.endDate = null")
-    Page<StatisticsUnit> getActualItems(String username, Long from, Pageable pageable);
+    @Query(value = "select o from StatisticsUnit o where o.user.username = :username and o.startDate >= :from and o.endDate = null")
+    Page<StatisticsUnit> getActualItems(@Param("username") String username,
+                                        @Param("from") Long from,
+                                        Pageable pageable);
 }
