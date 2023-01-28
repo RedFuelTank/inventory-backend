32b55ee838446c52555167fd374a534e7851c53e
Danila Romanov <danila.romanof@gmail.com>
2023-01-28 16:09:34 +0200

    Validation for entityDto has been added + handler for validation exceptions

--- src/main/java/dto/directory/EntityDto.java
+++ src/main/java/dto/directory/EntityDto.java
@@ -1,5 +1,8 @@
 package dto.directory;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +15,13 @@ import lombok.Setter;
 public abstract class EntityDto {
     private Long id;
     private EntityType type;
+    @NotNull
+    @Size(min = 1)
     private String name;
+    @NotNull
+    @Size(min = 1)
     private String username;
+    @Min(0)
     private Long parentDirectoryId;
 
     public enum EntityType {
--- src/main/java/exception/ExceptionAdvice.java
+++ src/main/java/exception/ExceptionAdvice.java
@@ -1,12 +1,28 @@
 package exception;
 
+import exception.validation.ValidationErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
-    @ExceptionHandler(Exception.class)
-    public String onException() {
-        return "internal error!";
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrors validate(MethodArgumentNotValidException exception) {
+        var fields = exception.getBindingResult().getFieldErrors();
+
+        ValidationErrors validationErrors = new ValidationErrors();
+
+        fields.forEach(validationErrors::addFieldError);
+
+        return validationErrors;
     }
+
+//    @ExceptionHandler(Exception.class)
+//    public String onException() {
+//        return "internal error!";
+//    }
 }
