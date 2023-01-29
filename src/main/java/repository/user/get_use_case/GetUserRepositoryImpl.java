c4c1941fa979619cfe1243be9db17c068e7465b4
Danila Romanov <danila.romanof@gmail.com>
2023-01-29 15:18:21 +0200

    Throw NotFoundException if user not found

--- src/main/java/repository/user/get_use_case/GetUserRepositoryImpl.java
+++ src/main/java/repository/user/get_use_case/GetUserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package repository.user.get_use_case;
 
+import exception.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import model.user.User;
@@ -8,12 +9,19 @@ import service.user.get_use_case.GetUserRepository;
 
 @Repository
 public class GetUserRepositoryImpl implements GetUserRepository {
-
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
     public User getUserByUsername(String username) {
-        return entityManager.find(User.class, username);
+        User user = entityManager.find(User.class, username);
+
+        if (user == null) {
+            throw new NotFoundException(
+                    String.format("User with username (%s) doesn't exist", username)
+            );
+        }
+
+        return user;
     }
 }
