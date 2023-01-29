7db41a5f3fd78251e1ee4be7433381e2cbe84d32
Danila Romanov <danila.romanof@gmail.com>
2023-01-29 10:22:50 +0200

    Validation has been added

--- src/main/java/dto/directory/EntityDto.java
+++ src/main/java/dto/directory/EntityDto.java
@@ -1,5 +1,6 @@
 package dto.directory;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public abstract class EntityDto {
     private Long id;
+    @NotNull
     private EntityType type;
     @NotNull
     @Size(min = 1)
--- src/main/java/dto/image/ImageDataDto.java
+++ src/main/java/dto/image/ImageDataDto.java
@@ -1,5 +1,9 @@
 package dto.image;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,7 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Setter
 public class ImageDataDto {
+    @NotNull
+    @Size(min = 1)
     private String username;
-    private Long itemId;
+    @Min(0)
+    private long itemId;
+    @NotNull
     private MultipartFile file;
 }
