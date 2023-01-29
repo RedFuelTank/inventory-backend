5cb73876c1b9bf51739d620ebbba65ff9fa31d96
Danila Romanov <danila.romanof@gmail.com>
2023-01-29 10:24:31 +0200

    Validation and field type has been changed for startDate and endDate from Long to Date

--- src/main/java/dto/statistics/StatisticsUnitDto.java
+++ src/main/java/dto/statistics/StatisticsUnitDto.java
@@ -1,14 +1,29 @@
 package dto.statistics;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
 public class StatisticsUnitDto {
     private Long id;
+    @NotNull
+    @Size(min = 1)
     private String name;
+    @NotNull
+    @Size(min = 1)
+    private String username;
+    @Min(0)
     private Long itemId;
-    private Long startDate;
-    private Long endDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date endDate;
 }
